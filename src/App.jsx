5121c88966c00227b0fd0bce640d0d381f38c66e
Lalit Kumar <lalitchauhan1517@gmail.com>
2025-02-25 22:10:36 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,8 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import { useEffect } from "react";
 import Routers from "./Routes/Routes";
 import ScrollToTop from "./Components/ScrollToTop";
-import { messaging } from "./firebase";
-import { getToken } from 'firebase/messaging';
 
 function App() {
-
-  async function requestPermission() {
-    const permission = await Notification.requestPermission();
-
-    if (permission === "granted") {
-      const token = await getToken(messaging, { vapidKey: 'BBwTjx9MN408kbDq1N9xtkgO6VonMhrKgFJuXjSlHfrr3VawLe8Y7rSm7uEgGes_qB9YOc5knmE7KIbPp0jA_sQ' });
-
-      if (token) {
-        console.log("Device token obtained successfully");
-
-        const apiUrl = "https://notification-marketing-hudbil.onrender.com/api/save-device-token";
-
-        const userData = {
-          deviceToken: token,
-        };
-
-        try {
-          const response = await fetch(apiUrl, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(userData),
-          });
-
-          if (response.ok) {
-            console.log("Device token sent to server successfully.");
-          } else {
-            console.error("Failed to send device token to backend.");
-          }
-        } catch (error) {
-          console.error("Error sending device token:");
-        }
-      }
-    } else if (permission === "denied") {
-      console.log("Notification permission denied");
-    }
-  }
-
-  useEffect(() => {
-    setTimeout(() => {
-    requestPermission();
-    }, 2000)
-  }, []);
 
   return (
     <>
@@ -62,3 +15,68 @@ function App() {
 }
 
 export default App;
+
+// import { BrowserRouter as Router } from "react-router-dom";
+// import { useEffect } from "react";
+// import Routers from "./Routes/Routes";
+// import ScrollToTop from "./Components/ScrollToTop";
+// import { messaging } from "./firebase";
+// import { getToken } from 'firebase/messaging';
+
+// function App() {
+
+//   async function requestPermission() {
+//     const permission = await Notification.requestPermission();
+
+//     if (permission === "granted") {
+//       const token = await getToken(messaging, { vapidKey: 'BBwTjx9MN408kbDq1N9xtkgO6VonMhrKgFJuXjSlHfrr3VawLe8Y7rSm7uEgGes_qB9YOc5knmE7KIbPp0jA_sQ' });
+
+//       if (token) {
+//         console.log("Device token obtained successfully");
+
+//         const apiUrl = "https://notification-marketing-hudbil.onrender.com/api/save-device-token";
+
+//         const userData = {
+//           deviceToken: token,
+//         };
+
+//         try {
+//           const response = await fetch(apiUrl, {
+//             method: "POST",
+//             headers: {
+//               "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify(userData),
+//           });
+
+//           if (response.ok) {
+//             console.log("Device token sent to server successfully.");
+//           } else {
+//             console.error("Failed to send device token to backend.");
+//           }
+//         } catch (error) {
+//           console.error("Error sending device token:");
+//         }
+//       }
+//     } else if (permission === "denied") {
+//       console.log("Notification permission denied");
+//     }
+//   }
+
+//   useEffect(() => {
+//     setTimeout(() => {
+//     requestPermission();
+//     }, 2000)
+//   }, []);
+
+//   return (
+//     <>
+//       <Router>
+//         <ScrollToTop />
+//         <Routers />
+//       </Router>
+//     </>
+//   );
+// }
+
+// export default App;
